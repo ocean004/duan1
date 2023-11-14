9551d0fd590148384a7d9226a968722bd4fd98ed
ocean004 <pphu0936@gmail.com>
2023-11-14 22:25:12 +0700

    update

---
+++ model/taikhoan.php
@@ -0,0 +1,26 @@
+<?php
+    function insert_taikhoan($email,$user,$pass){
+        $sql = "INSERT INTO taikhoan(email,user,pass) VALUES('$email','$user','$pass')";
+        pdo_execute($sql);
+    }
+    function checkuser($user,$pass){
+        $sql = "select * from taikhoan where user = '".$user."' AND pass = '".$pass."'";
+        $result = pdo_query_one($sql);
+        return $result;
+    }
+    function checkemail($email){
+        $sql = "select * from taikhoan where email = '".$email."'";
+        $result = pdo_query_one($sql);
+        return $result;
+    }
+    function update_taikhoan($id,$user,$pass,$email,$address,$tel){
+        $sql = "UPDATE taikhoan SET user='".$user."', pass='".$pass."', email='".$email."', address='".$address."', tel='".$tel."' WHERE id=".$id;
+        pdo_execute($sql);
+        
+    }
+    function loadall_taikhoan(){
+        $sql="select * from taikhoan order by id desc";
+        $listtaikhoan=pdo_query($sql);
+        return  $listtaikhoan;
+    }
+?>
